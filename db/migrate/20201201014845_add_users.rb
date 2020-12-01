61c3d2a777256e494b016f188631fa4223b9f7fe
root <root@DESKTOP-8D6COP4.localdomain>
2020-11-30 20:04:02 -0600

    Feature 1: - Enseñando

---
+++ db/migrate/20201201014845_add_users.rb
@@ -0,0 +1,10 @@
+class AddUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+ 
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20201201015549_create_empleados.rb
@@ -0,0 +1,8 @@
+class CreateEmpleados < ActiveRecord::Migration[6.0]
+  def change
+    create_table :empleados do |t|
+      t.string :nombre
+      t.timestamps
+    end
+  end
+end
