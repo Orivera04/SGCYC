5412d07d5673edbae15f374716066ffbe31efab4
root <root@DESKTOP-8D6COP4.localdomain>
2020-12-09 19:55:33 -0600

    Assets

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module Nicacentro
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.assets.initialize_on_precompile = false
-    config.assets.enabled = true
   end
 end
