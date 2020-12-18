de1f515dc341ece7956f091894a22dc84d063b87
JChavezNi <josue.chavez.ni@gmail.com>
2020-12-17 18:07:53 -0600

    add routes: socio

--- app/controllers/socios_controller.rb
+++ app/controllers/socios_controller.rb
@@ -10,4 +10,22 @@ class SociosController < ApplicationController
         @socio = Socio.new
     end
 
+    def create
+        @socio = Socio.new(socio_params)
+        if @socio.save
+            redirect_to socios_path
+            #redirect_to :action => 'list'
+        else
+            #flash.now[:error] = "Could not save socio"
+            render :new 
+            #@socios = Socio.all
+            #render :action => 'new'
+        end
+    end
+
+    #private
+    def socio_params 
+        params.require(:socio).permit(:codigo_socio, :nombre, :apellido, :numero_cedula, :numero_ruc, :direccion, :telefono, :correo, :estado)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   #resources :socios
   get '/socios', to: 'socios#index'
   get '/socios/new', to: 'socios#new'
+  post '/socios', to: 'socios#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
