4fb1ed0b7f0fd3c69f782d7f575aba1e4a505580
Oscar Rivera <orivera@DESKTOP-8D6COP4.localdomain>
2020-12-29 23:09:20 -0600

    - Se implementa totalmente los cambios de la interfaz

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+//= require jquery.min.js
+//= require bootstrap.bundle.min.js
+//= require js.cookie.js
+//= require jquery.scrollbar.min.js
+//= require jquery-scrollLock.min.js
+//= require argon.js
